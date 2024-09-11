db403f151e2f74d1206e5f5f05b79a00865a363f
impooya <samarkarpooya@gmail.com>
2024-09-12 01:28:02 +0330

    remove backend file

--- Coffee_store_api_django-main/store/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Coffee
-
-
-class CoffeeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Coffee
-        fields = "__all__"
--- Coffee_store_api_django-main/store/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import CoffeeView
-
-urlpatterns = [
-    path("coffee/", CoffeeView.as_view()),
-    path("coffee/<int:pk>/", CoffeeView.as_view()),
-]
